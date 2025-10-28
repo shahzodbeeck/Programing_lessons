8247665ce72836096a024c896dc2f1e765749908
shahzodbeeck <shahzodomonboyev1@gmail.com>
2025-10-28 12:44:42 +0500

    doing lesson task

--- main.py
+++ main.py
@@ -391,47 +391,66 @@
 # print(tuple(tup_2))
 
 # task1
-numbers =[10,20,30,40,50]
-numbers[0]=100
+numbers = [10, 20, 30, 40, 50]
+numbers[0] = 100
 print(numbers)
 numbers.append(60)
 print(numbers)
 print(len(numbers))
-#task 2
-fruits =["appl;e","banana","cherry"]
-fruits[-1:-1] =['orange','kiwi']
+# task 2
+fruits = ["appl;e", "banana", "cherry"]
+fruits[-1:-1] = ['orange', 'kiwi']
 print(fruits)
-fruits[1]='mango'
+fruits[1] = 'mango'
 fruits.sort()
 print(fruits)
-#task 3
-students =["Ali","olim","zarina","jasur","sabina"]
+# task 3
+students = ["Ali", "olim", "zarina", "jasur", "sabina"]
 print(students.index('zarina'))
 
-#task 4
-my_tuple =5,10,15,20,25
+# task 4
+my_tuple = 5, 10, 15, 20, 25
 print(my_tuple[2])
 print(len(my_tuple))
 # task 5
-my_tuple =1,2,3
-my_list =[1,2,3]
+my_tuple = 1, 2, 3
+my_list = [1, 2, 3]
 my_list.append(4)
 """we cannot append tuple beacuse is unmutable"""
 """list mutable tuple unmutable ,we need breakets for create list but tuple only need comma"""
 
-#task 6
+# task 6
 
-colors ='red','green','blue'
-colors =list(colors)
+colors = 'red', 'green', 'blue'
+colors = list(colors)
 colors.append('yellow')
 print(tuple(colors))
 
-#Task 7
-numbers=[5,10,15,20,25]
+# Task 7
+numbers = [5, 10, 15, 20, 25]
 numbers.append(30)
 numbers.reverse()
 print(numbers)
-my_tuple_2 =10,20,30,40
-my_tuple_2 =list(my_tuple_2)
+my_tuple_2 = 10, 20, 30, 40
+my_tuple_2 = list(my_tuple_2)
 my_tuple_2.reverse()
 print(list(my_tuple_2))
+# task 8
+num_tuple = (50, 60, 70, 80)
+num_list = [10, 20, 30, 40]
+num_list.extend(num_tuple)
+print("MIn", min(num_list))
+print("Max", max(num_list))
+# task 9
+nested_tuple = (1, 2, (3, 4, 5), 6, 7)
+print(nested_tuple[2])
+for i in nested_tuple:
+    if type(i) == tuple:
+        for j in i:
+            print(j)
+    else:
+        print(i)
+# task 10
+numbers = [2, 4, 6, 8, 10]
+new_list = list(map(lambda x: x * 2, numbers))
+print(new_list)
